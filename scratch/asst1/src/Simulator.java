d81a2aa977335aa64bb4ffc90ec1077dbaa401cd
Ryan Fish <fishr@mit.edu>
2015-02-17 16:09:06 -0500

    lots of tests completed, and importantly the code works for pentagon at least

--- scratch/asst1/src/Simulator.java
+++ scratch/asst1/src/Simulator.java
@@ -6,6 +6,7 @@ public class Simulator {
 	private int sec;
 	private int msec;
 	private int sides;
+	private long loop_time;
 	private Hashtable<Long, Control> bookmarks;  //stores controls keyed by the hundreths of seconds
 	
 	private final int MIN_SIDES = 3;
@@ -32,7 +33,7 @@ public class Simulator {
 	}
 	
 	Control getControl(int sec, int msec){
-		return bookmarks.get((long)(sec*100+msec));
+		return bookmarks.get(((long)(sec*100+msec/10))%this.loop_time);
 	}
 	
 	int setNumSides(int n){
@@ -40,7 +41,7 @@ public class Simulator {
 		if(util.withinBounds(n, MIN_SIDES, MAX_SIDES)){
 			sides=n;
 		}
-		double dTheta = (Math.PI-((sides-2)*Math.PI/this.sides))/2;
+		double dTheta = (Math.PI-((sides-2)*Math.PI/sides))/2;
 		long half_turn = Math.round(Math.ceil(dTheta*100.0/GroundVehicle.MAX_THETA_DOT));
 		double turn_rate = dTheta*100/half_turn;
 		double natural_side_length = 50.0*Math.sin(Math.PI/sides);
@@ -50,14 +51,18 @@ public class Simulator {
 		long straight_time = Math.round(Math.ceil(side_length_remainder*100.0/GroundVehicle.MAX_S_DOT));
 		double straight_rate = side_length_remainder*100/straight_time;
 		
-		System.out.format("%.2f for %d and theta %.2f for %d%n", turn_rate, half_turn, straight_rate, straight_time);
+		//System.out.format("%.2f for %d and theta %.2f for %d%n", turn_rate, half_turn, straight_rate, straight_time);
 		
 		for(int i = 0; i<sides; i++){
 			long offset_t = 2*i*half_turn+i*straight_time;
+			assert(half_turn>0);
+			assert(straight_time>0);
 			bookmarks.put(offset_t, new Control(GroundVehicle.MIN_S_DOT, turn_rate));
 			bookmarks.put(offset_t+half_turn, new Control(straight_rate, 0));
 			bookmarks.put(offset_t+half_turn+straight_time, new Control(GroundVehicle.MIN_S_DOT, turn_rate));
 		}
+		
+		this.loop_time = sides*(2*half_turn+straight_time);
 		
 		return sides;
 	}
@@ -71,7 +76,7 @@ public class Simulator {
 			this.vehicle.controlVehicle(this.getControl(this.sec, this.msec));
 			this.vehicle.updateState(0, SIM_STEP);
 			double[] pose = this.vehicle.getPosition();
-			System.out.format("%.2f %.2f %.2f %.1f%n", this.sec+this.msec/1000.0, pose[0], pose[1], pose[2]*180.0/Math.PI);
+			System.out.format("%.2f %.2f %.2f %.1f %.2f %.2f%n", this.sec+this.msec/1000.0, pose[0], pose[1], pose[2]*180.0/Math.PI, this.vehicle.getRawVelocity()[0], this.vehicle.getRawVelocity()[1]);
 			msec+=SIM_STEP;
 			if(this.msec==1000){
 				this.msec=0;
@@ -79,7 +84,7 @@ public class Simulator {
 			}
 		}
 		
-		System.out.println("finished sim, exiting...");
+		//System.out.println("finished sim, exiting...");
 	}
 	
 	public static void main(String argv[]){
