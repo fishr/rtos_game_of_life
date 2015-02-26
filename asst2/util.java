64b55d3798d511ae269b10f5209c7c9c6a6e53ba
Ryan Fish <fishr@mit.edu>
2015-02-26 14:05:49 -0500

    oh, should probably make sure it compiles

---
+++ asst2/util.java
@@ -0,0 +1,30 @@
+
+public class util {
+
+	public static double clampDouble(double input, double lower, double upper){
+		assert(lower<=upper);
+		return Math.max(Math.min(input, upper), lower);
+	}
+	
+	public static int clampInt(int input, int lower, int upper){
+		assert(lower<=upper);
+		return Math.max(Math.min(input, upper), lower);
+	}
+
+	public static double wrapAngle(double d) {
+		double tempTheta = (d%(2*Math.PI));
+		if(tempTheta>=Math.PI){
+			tempTheta=tempTheta-(2*Math.PI);
+		}
+		assert((tempTheta>=-Math.PI)&&(tempTheta<Math.PI));
+		return tempTheta;
+	}
+	
+	public static boolean withinBounds(double input, double lower, double upper){
+		return (clampDouble(input, lower, upper)==input);
+	}
+	
+	public static boolean withinBounds(int input, int lower, int upper){
+		return (clampInt(input, lower, upper)==input);
+	}
+}
