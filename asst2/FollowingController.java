215a841a0553d6ce8303021f59f3ebd8d2bf83cc
Ryan Fish <fishr@mit.edu>
2015-02-26 14:04:29 -0500

    final commit for assignment

---
+++ asst2/FollowingController.java
@@ -0,0 +1,8 @@
+
+public class FollowingController extends VehicleController {
+
+	public FollowingController(Simulator s, GroundVehicle gv, GroundVehicle leader){
+		super(s, gv);
+		//TODO
+	}
+}
---
+++ asst2/RandomController.java
@@ -0,0 +1,9 @@
+
+public class RandomController extends VehicleController
+{
+
+	@Override
+	public Control getControl(int secs, int msecs){
+		//TODO
+	}
+}
