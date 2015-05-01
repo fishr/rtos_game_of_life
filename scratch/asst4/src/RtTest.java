676e3fafd4c6c5ca97699d24f3207b8710f0e466
Ryan <fishr@mit.edu>
2015-04-30 22:15:55 -0400

    oops, one using finl one using temp folders

--- scratch/asst4/src/RtTest.java
+++ scratch/asst4/src/RtTest.java
@@ -2,7 +2,7 @@ import javax.realtime.*;
 
 public class RtTest {
 
-	private static boolean rt=false;
+	private static boolean rt=true;
 
 	public static void main(String argv[]){
 		Simulator sim = new Simulator();
@@ -16,42 +16,46 @@ public class RtTest {
 				GroundVehicle gv = new GroundVehicle(temp, 10*Math.random(), Math.PI/2*(Math.random()-1/2), sim, true);
 				sim.addCircleVehicle(gv);
 				
-				if(argv.length>1){
-					if(argv[1].equalsIgnoreCase("realtime")){
-						rt=true;
-					}else{
-						throw new IllegalArgumentException("please input the number of vehicles");
-					}
-				}
 			}
 			catch(NumberFormatException e){
 				System.out.println("ip address?");
 			}
 		}
-				
+
+
+		System.out.println("about to enter real time"); 				
 		
 		if(rt){
-			PriorityParameters prip = new PriorityParameters(PriorityScheduler.instance().getNormPriority());
+			System.out.println("just before priorities");	
+			//PriorityParameters prip = new PriorityParameters(PriorityScheduler.instance().getNormPriority());
+			System.out.println("just after");
 			for(VehicleController v : sim.vehicles){
+				System.out.println("is this thing on?");
 				RealtimeThread realCont = new RealtimeThread(null, null, null, null, null, v);
 				PeriodicParameters pp1 = new PeriodicParameters(null, new RelativeTime(1,0), null, null,new OverrunHand(realCont),new MissHand(realCont));
-				realCont.setSchedulingParameters(prip); 
+				//realCont.setSchedulingParameters(prip); 
 				realCont.setReleaseParameters(pp1);
+				realCont.schedulePeriodic();
 
 				RealtimeThread realVeh = new RealtimeThread(null, null, null, null, null, v.getVehicleRun());
 				PeriodicParameters pp2 = new PeriodicParameters(null, new RelativeTime(5,0), null, null,new OverrunHand(realVeh),new MissHand(realVeh));
-				realCont.setSchedulingParameters(prip); 
-				realCont.setReleaseParameters(pp2);
-			}
+				//realVeh.setSchedulingParameters(prip); 
+				realVeh.setReleaseParameters(pp2);
+				realVeh.schedulePeriodic();			
+}
 			
 			RealtimeThread realSim = new RealtimeThread(null, null, null, null, null, sim);
-			PeriodicParameters pp1 = new PeriodicParameters(null, new RelativeTime(100,0), null, null, new OverrunHand(realSim), new MissHand(realSim));
-			realSim.setSchedulingParameters(prip); 
-			realSim.setReleaseParameters(pp1);
-			
-			try{
+			PeriodicParameters pp3 = new PeriodicParameters(null, new RelativeTime(5,0), null, null, new OverrunHand(realSim), new MissHand(realSim));
+			//realSim.setSchedulingParameters(prip); 
+			realSim.setReleaseParameters(pp3);
+			realSim.schedulePeriodic();
+
+			System.out.println("things should be scheduled");
+
+	
+			try {
 				realSim.join();
-			} catch (InterruptedException e) {
+			}catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}else{
